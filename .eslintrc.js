09786f486ed41b7e377a68d76c2b30c97988a7c5
Daniel Carrizales <osvaldo.delgadofl@gmail.com>
2020-02-20 10:45:09 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["airbnb-typescript"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "linebreak-style": 0,
+    }
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+## Steps to install the project
+1. Clone this repository.
+2. Install the modules *npm install*
+3. Add the extension *ESLint* to *Visual Studio*
+4. Add a new shortcut: *CTRL+SHIFT+P* and *Preferences: Open Keyboard Shortcut(JSON)* add in the array of objects: 
+  *"key": "alt+shift+p"*
+  *"command": "eslint.executeAutofix"*
+
+To start the project use: *npm start*.
